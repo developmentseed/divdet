71412b98c618686a597923ee2fc30d205fd61fa7
wronk <wronk@developmentseed.org>
2020-07-25 11:23:36 -0700

    Removing test as its likely obsolete

--- test/test_surface_feature.py
+++ test/test_surface_feature.py
@@ -8,19 +8,11 @@ import unittest
 
 import numpy as np
 
-from divdet.surface_feature import SurfaceFeature
+#from divdet.surface_feature import SurfaceFeature
 
 class Test(unittest.TestCase):
     """Test surface feature functionality"""
-
-    def test_surface_feature(self):
-        """Test that valid corners are calculated correctly when cropping"""
-
-        # Fully valid image
-        sf1 = SurfaceFeature(1, 1, 2, 2, 'dummy_wkt_string', 0.5, 'dummy_id')
-        sf1.determine_quadkey()
-
-        self.assertEqual(sf1.quadkey, '3000000')
+    pass
 
 
 if __name__ == '__main__':
